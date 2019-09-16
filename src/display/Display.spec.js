40bf636d6d5eae49a4acbec86711abba1f4941c7
John Rojcewicz <rojcewiczj@gmail.com>
2019-09-16 16:16:23 -0500

    locked and closed, rendering basic components

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display'
+
+test ('locked and closed are set to false', () => {
+    render(<Display />)
+    expect(Display.defaultProps.closed).toBe(false);
+    expect(Display.defaultProps.locked).toBe(false);
+})
